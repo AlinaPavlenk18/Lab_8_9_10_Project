c62dc32002bc03c469fcaa3bced3afe109939cab
AlinaPavlenk18 <alinapavlenk01005@gmail.com>
2025-04-06 16:16:08 +0300

    Added MovieList component to main App.jsx file and styled it and the page

--- cinema-app/src/App.jsx
+++ cinema-app/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { movies as allMovies } from './data/movies';
+import MovieList from '../src/components/MovieList';
+import './index.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [search, setSearch] = useState("");
+
+  const filteredMovies = allMovies.filter(movie =>
+    movie.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app">і
+      <input
+        type="text"
+        placeholder="Пошук за назвою..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="search-input"
+      />
+      <MovieList movies={filteredMovies} />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- cinema-app/src/components/MovieCard.jsx
+++ cinema-app/src/components/MovieCard.jsx
@@ -1,4 +1,4 @@
-const MovieCard = ({ title, description, genre, time, poster}) => {
+const MovieCard = ({ title, description, genre, date, time, poster}) => {
     return (
       <div className="movie-card">
         <img src={poster} alt={title} />
@@ -6,6 +6,7 @@ const MovieCard = ({ title, description, genre, time, poster}) => {
           <h2>{title}</h2>
           <p>{description}</p>
           <p><strong>Жанр:</strong> {genre}</p>
+          <p><strong>Дата:</strong> {date}</p>
           <p><strong>Сеанс:</strong> {time}</p>
         </div>
       </div>
