6a57c94631f5fa303600840ab28afda3fc37bc24
AlinaPavlenk18 <alinapavlenk01005@gmail.com>
2025-04-07 16:24:42 +0300

    Added and stylized sidebar with filters

--- cinema-app/src/App.jsx
+++ cinema-app/src/App.jsx
@@ -2,19 +2,45 @@ import { useState } from 'react';
 import { movies as allMovies } from './data/movies';
 import MovieList from '../src/components/MovieList';
 import Header from "../src/components/Header";
+import Sidebar from "../src/components/Sidebar";
 import './index.css';
 
 const App = () => {
   const [search, setSearch] = useState("");
+  const [genre, setGenre] = useState("Усі");
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  // Витягуємо унікальні жанри
+  const genres = ["Усі", ...new Set(allMovies.map((m) => m.genre.split(", ")[0]))];
+   // Фільтрація за назвою та жанром
+   const filteredMovies = allMovies.filter((movie) => {
+    const matchesTitle = movie.title.toLowerCase().includes(search.toLowerCase());
+    const matchesGenre = genre === "Усі" || movie.genre.includes(genre);
+    return matchesTitle && matchesGenre;
+  });
 
-  const filteredMovies = allMovies.filter(movie =>
-    movie.title.toLowerCase().includes(search.toLowerCase())
-  );
+  const handleGenreChange = (newGenre) => {
+    setGenre(newGenre);
+    setSidebarOpen(false); // Автоматичне закриття після вибору
+  };
 
   return (
     <>
     <Header/>
     <div className="app">
+      {!sidebarOpen && (
+          <button className="toggle-sidebar-btn" onClick={() => setSidebarOpen(true)}>
+            ☰ Фільтри
+          </button>
+        )}
+
+        {sidebarOpen && <div className="overlay" onClick={() => setSidebarOpen(false)}></div>}
+
+        <Sidebar
+          isOpen={sidebarOpen}
+          genres={genres}
+          selectedGenre={genre}
+          onGenreChange={handleGenreChange}
+        />
       <input
         type="text"
         placeholder="Пошук за назвою..."
---
+++ cinema-app/src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+
+import React from "react";
+import "../styles/Sidebar.css";
+
+const Sidebar = ({ genres, selectedGenre, onGenreChange, isOpen }) => {
+  return (
+    <aside className={`sidebar ${isOpen ? "open" : ""}`}>
+      <h3>🎞️ Жанри</h3>
+      <ul>
+        {genres.map((g) => (
+          <li
+            key={g}
+            className={g === selectedGenre ? "active" : ""}
+            onClick={() => onGenreChange(g)}
+          >
+            {g}
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
